abc0d0189132aa8721c216d9d85809dc943e2e03
Deepanshugarg11 <dipanshugarg13@gmail.com>
2024-09-15 13:41:42 +0530

    Add files via upload

---
+++ src/Components/Bar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Bar.css';
+
+function CenteredBar() {
+  return (
+    <div className="CenterBar">
+      <p>New Post</p>
+    </div>
+  );
+}
+
+export default CenteredBar;
